2c844964fb1df0ab34c220a1b36c4fa9f02c8f41
Crimsonet <aether54@protonmail.com>
2025-01-07 15:27:30 +0800

    Jan 07,2025:1525

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,14 +1,5 @@
 function fish_prompt
-    if test -n "$SSH_TTY"
-        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
-    end
-
-    echo -n (set_color blue)(prompt_pwd)' '
-
-    set_color -o
-    if fish_is_root_user
-        echo -n (set_color red)'# '
-    end
-    echo -n (set_color 0056ff)'❯'(set_color 2e8aff)'❯'(set_color 63a8ff)'❯ '
+    echo -n (set_color ff8a00)(prompt_pwd)' '
+    echo -n (set_color ff6c00)'诏❯ '
     set_color normal
 end
