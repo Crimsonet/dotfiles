4136df40fd75f0d167cc814eb554f53812784514
Crimsonet <crimsonet@tuta.io>
2025-01-11 22:14:44 +0800

    Jan 11,2025:2214

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,10 +1,9 @@
 function fish_prompt
     echo -n (set_color ff8a00)(prompt_pwd)' '
     if fish_is_root_user
-        echo -n (set_color e50000)'诏'
+        echo -n (set_color e50000)'诏: '
     else
-        echo -n (set_color ff6c00)'谕'
+        echo -n (set_color ff6c00)'谕: '
     end
-    echo -n (set_color ff6c00)': '
     set_color normal
 end
